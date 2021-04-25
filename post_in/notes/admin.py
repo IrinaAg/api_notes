429806f24448e2767ab804fc1056c814276736db
agafonova <la_caterina@mail.ru>
2021-04-25 22:57:21 +0300

    Added job site.

--- post_in/notes/admin.py
+++ post_in/notes/admin.py
@@ -5,3 +5,16 @@ from .models import Note
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     list_display = ['title', 'text', 'created']
+
+numbers = [0, 1]
+odd_numbers = []
+while numbers[0] < 1000000:
+    need = numbers[0] + numbers[1]
+    numbers[0], numbers[1] = numbers[1], need
+    if (need % 2) == 0:
+        odd_numbers.append(need)
+        # print(odd_numbers)
+print(odd_numbers[:5])
+
+
+
